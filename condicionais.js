8f6bd580c3dbd2c5bba86ac924fed600d1853e96
lucas <silvalucas74275@gmail.com>
2025-04-03 21:51:38 -0300

    2 fase js

---
+++ condicionais.js
@@ -0,0 +1,51 @@
+// let numero = prompt("digite um numero:");
+
+// if (numero > 0) {
+
+// console.log("positivo.")
+
+// }else {
+
+// if( numero < 0) {
+
+// console.log("negativo.")
+
+// }else {
+
+// console.log(" e zero")
+
+// }
+
+
+
+// let numero = prompt("qual e sua idade");
+
+// if(numero > 18); {
+
+//     console.log("maior de idade");
+// }else {
+
+// if( numero < 18) {
+
+// console.log("menor de idade")
+
+// }
+// }
+
+let numero = prompt("digite sua idade");
+
+if (numero > 18) {
+
+console.log("positivo.")
+else {
+
+if( numero < 18) {
+
+console.log("negativo.")
+
+}else {
+
+console.log(" e zero")
+
+}
+}
--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-alert("helo world na tela");
+// alert("helo world na tela");
 
-console.log("helo world");
+// console.log("helo world");
+
+let nome = "lucas";
+const idade = 22;
+var cidade = "sao paulo"
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+console.log("meu nome e: " + nome); //concatenacao
+console.log("minha idade e: " + idade);
+console.log("minha cidade e: " + cidade);
+
+console.log("meu nome e: " + nome);
+console.log(`meu nome e: ${nome}`); //interpolacao
+
+console.log("minha idade e: " + idade);
+console.log(`minha ${idade} anos`);
+
+console.log("minha cidade e: " + cidade);
+console.log(`minha cidade e: ${cidade}`);
+
+console.log(typeof idade)
+
+let n1 = "10";
+let n2 = 10;
+
+let soma = Number(n1) + n2;
+
+console.log(soma);
